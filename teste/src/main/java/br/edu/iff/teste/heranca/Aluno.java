13e2e28dc0058da49dcc0a359f31e384e78c334a
ArthurF22 <arthur132f@gmail.com>
2019-09-26 09:50:07 -0300

    Adicionando o metodo diploma a classe Aluno

--- teste/src/main/java/br/edu/iff/teste/heranca/Aluno.java
+++ teste/src/main/java/br/edu/iff/teste/heranca/Aluno.java
@@ -14,6 +14,8 @@ public class Aluno {
     Date dtNascimento;
     long matricula, rendaFamiliar;
     Endereco endResidencial;
+    boolean podeSolicitarDiploma;
+    boolean concluiuasdiciplinas;
     
     public int idade() {
         Date hoje = new Date();
@@ -30,6 +32,12 @@ public class Aluno {
         } else {
             return anos;
         }
+    }
+            
+        public boolean podeSolicitarDiploma (){
+            return concluiuasdiciplinas && podeSolicitarDiploma;
+            
+        }
         
     }
-}
+
